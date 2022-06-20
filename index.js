8266bea5d0a643656dcbc829f84f48313c4c4478
IvaanOne <ivaanone17@gmail.com>
2022-06-20 13:13:36 +0200

    feat: dotenv added

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+const port = process.env.PORT || 4000;
+
+// connection to mongodb
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> {
+    console.log("connection stablished");
+})
+.catch((error)=> {
+    console.log('Error connecting to MongoDB', error);   
+});
+
 
 
 //routes
 app.get('/', (req, res) => {
     return res.send('Bienvenidos a mi aplicacion de tareas');
 });
-app.listen(3000, () => {console.log("server is running")});
+app.listen(port, () => {console.log("server is running: " + port)});
